7ae235937dd6d44f9b8a2063a1c8d813c4f8e7a2
Elisa Pace <s1084405@studenti.univpm.it>
2021-08-24 18:37:30 +0200

    modifica quiz_bottoni, stringhe e test_conoscenze_valuatzione

--- app/src/main/java/com/example/kotlinlearning/MainActivity.kt
+++ app/src/main/java/com/example/kotlinlearning/MainActivity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.completamento_quiz)
+        setContentView(R.layout.test_conoscenze_valutazione)
 
     }
 
