c146eec23c2ab7daa0f24e477fdd078fa137ed7f
WideHardo7 <marzilianoluca@yahoo.com>
2021-09-07 20:09:57 +0200

    Pog-ress aggiunta del navigation drawer ( modifica activity_main.xml, aggiunta nav_header.xml, aggiunta nav_menu.xml, modifica glossary in strings.xml)

--- app/src/main/java/com/example/kotlinlearning/MainActivity.kt
+++ app/src/main/java/com/example/kotlinlearning/MainActivity.kt
@@ -2,24 +2,39 @@ package com.example.kotlinlearning
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.navigateUp
+import com.example.kotlinlearning.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        /*val inten = Intent(this,ArgomentoFragment::class.java)
-        val button:Button= findViewById(R.id.b_variabili)
-        button.setOnClickListener{
-            startActivity(inten)
 
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 
+        val navController = this.findNavController(R.id.fragmentContainerView)
 
-        }  */
+        //supa action bar
+        drawerLayout = binding.drawerLayout
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+
+        //Navigation drawer
+        NavigationUI.setupWithNavController(binding.navView, navController)
 
 
     }
 
-
-
+    //app bar bottone up
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.fragmentContainerView)
+        return navController.navigateUp(drawerLayout)
+    }
 
 }
