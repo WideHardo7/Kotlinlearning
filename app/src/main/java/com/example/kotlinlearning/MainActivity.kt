3fff8242be634334d287e1b755bae9812490b69d
WideHardo7 <marzilianoluca@yahoo.com>
2021-09-13 18:30:41 +0200

    -Fixato layout achievements.xml -Aggiunto funzionamento firstTime in MainActivity.kt, differenzia il layout di origine in caso di primo boot dell'app -Aggiunto <layout> in test_conoscenze.xml

--- app/src/main/java/com/example/kotlinlearning/MainActivity.kt
+++ app/src/main/java/com/example/kotlinlearning/MainActivity.kt
@@ -1,44 +1,63 @@
 package com.example.kotlinlearning
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.onNavDestinationSelected
+import android.content.SharedPreferences
+import android.util.Log
 import com.example.kotlinlearning.databinding.ActivityMainBinding
+import com.example.kotlinlearning.databinding.TestConoscenzeBinding
 
 
 class MainActivity : AppCompatActivity() {
 
+    val preferences_name = "isFirstTime"
     private lateinit var drawerLayout: DrawerLayout
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-
-        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
-
-        val navController = this.findNavController(R.id.fragmentContainerView)
-
-        //supa action bar
-        drawerLayout = binding.drawerLayout
-        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
-
-        //Navigation drawer
-        NavigationUI.setupWithNavController(binding.navView, navController)
-
-
-
+        firstTime()
 
     }
 
     //app bar bottone up
     override fun onSupportNavigateUp(): Boolean {
+
         val navController = this.findNavController(R.id.fragmentContainerView)
         return navController.navigateUp(drawerLayout)
+
     }
 
+    fun firstTime() {
+
+        val sharedTime = getSharedPreferences(preferences_name,0)
+
+        if (sharedTime.getBoolean("firstTime",true)){
+
+            val binding = DataBindingUtil.setContentView<TestConoscenzeBinding>(this, R.layout.test_conoscenze)
+
+            sharedTime.edit().putBoolean("firstTime",false).apply()
+
+        } else {
+
+            binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+            val navController = this.findNavController(R.id.fragmentContainerView)
+
+            //supa action bar
+            drawerLayout = binding.drawerLayout
+            NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+
+            //Navigation drawer
+            NavigationUI.setupWithNavController(binding.navView, navController)
+
+        }
+    }
 }
+
