a40f170293335ac6149757d3f48d3e6ebfeb0999
WideHardo7 <marzilianoluca@yahoo.com>
2021-10-25 18:37:36 +0200

    -Aggiunto testing

--- app/src/androidTest/java/com/example/kotlinlearning/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/kotlinlearning/ExampleInstrumentedTest.kt
@@ -1,12 +1,24 @@
 package com.example.kotlinlearning
 
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.ViewAssertion
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.kotlinlearning.view.activity.MainActivity
+import com.example.kotlinlearning.view.activity.MainActivity2
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -21,4 +33,31 @@ class ExampleInstrumentedTest {
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.kotlinlearning", appContext.packageName)
     }
+}
+
+class MainButtonsTest {
+    @get:Rule
+    var activityScenarioRule: ActivityScenarioRule<MainActivity> = ActivityScenarioRule(MainActivity::class.java)
+    @Test
+    fun testBottoneVar() {
+        onView(withId(R.id.bottone_variabili)).perform(click())
+    }
+    @Test
+    fun testBottoneFun(){
+        onView(withId(R.id.bottone_funzioni)).perform(click())
+    }
+}
+
+class TestCon {
+    @get:Rule
+    var activityScenarioRule: ActivityScenarioRule<MainActivity2> = ActivityScenarioRule(MainActivity2::class.java)
+    @Test
+    fun testBottoneSalta(){
+        onView(withId(R.id.b_Salta)).perform(click())
+    }
+    @Test
+    fun testBottonePro(){
+        onView(withId(R.id.b_Procedi)).perform(click())
+    }
+
 }
